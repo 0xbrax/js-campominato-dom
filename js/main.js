5d5da0f6d95bcc3ec64a6f2f2ea8fc31b3267ec1
Marco Braccini <marco.braccini@live.it>
2022-07-14 17:08:34 +0200

    Working on game structure

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@ const playBtn = document.getElementById('play-btn');
 
 let gridSelection;
 
+let randomUniqueNumberList = [];
+const maxEndElement = 16;
+
 playBtn.addEventListener('click', 
 function() {
     if (gameMode.value == ''){
         alert('Scegli la difficoltà di gioco.');
     } else {
+        randomUniqueNumberList = [];
+        gridContainer.innerHTML = '';
+
         gridSelection = gameMode.value;
 
-        gridContainer.innerHTML = '';
+        randomUniqueNumber(maxEndElement, 1, gridSelection);
     
         for (let gridNumber = 1; gridNumber <= gridSelection; gridNumber++) {
     
@@ -22,9 +28,28 @@ function() {
         
             grid.addEventListener('click', 
             function() {
-                console.log(gridNumber);
-        
+                console.log(gridNumber)
+
+                let toogleVerify = false;
+
                 this.classList.toggle('grid-sec');
+
+                for (let i = 0; i < randomUniqueNumberList.length; i++) {
+                    if (gridNumber == randomUniqueNumberList[i]) {
+                        toogleVerify = true;
+
+                        console.log(toogleVerify)
+                    //grid.classList.add('grid-end');
+                    }
+                }
+                
+
+                if (toogleVerify == true) {
+                    alert('Hai perso.');
+
+                    
+                }
+
             });
         }
     
@@ -38,41 +63,35 @@ function() {
             gridMain[gridList].style.width = gridSizeFinal;
             gridMain[gridList].style.height = gridSizeFinal;
         }
+
+        console.log(randomUniqueNumberList)
     }
 });
 
 
 
-let numberList = [];
-let numberEndGame;
-let numberVerify = false;
+function randomUniqueNumber(maxEndElement, min, max) {
+    let numberEndGame;
+    let numberVerify = false;
 
-for (let list = 0; list < 16; list++) {
-    numberEndGame = randomNumber(1, 16);
-
-    if (numberList.includes(numberEndGame)) {
-        numberVerify = true;
-    }
-
-    while (numberVerify == true) {
-        numberEndGame = randomNumber(1, 16);
-
-        if (!numberList.includes(numberEndGame)) {
-            numberVerify = false;
+    for (let list = 0; list < maxEndElement; list++) {
+        numberEndGame = randomNumber(min, max);
+    
+        if (randomUniqueNumberList.includes(numberEndGame)) {
+            numberVerify = true;
         }
+    
+        while (numberVerify == true) {
+            numberEndGame = randomNumber(min, max);
+    
+            if (!randomUniqueNumberList.includes(numberEndGame)) {
+                numberVerify = false;
+            }
+        }
+    
+        randomUniqueNumberList.push(numberEndGame);
     }
-
-    numberVerify = false;
-
-    numberList.push(numberEndGame);
 }
-
-console.log(numberList)
-
-
-
-
-
 
 function randomNumber(min, max) {
     const number = Math.floor(Math.random() * (max - min + 1) + min);
