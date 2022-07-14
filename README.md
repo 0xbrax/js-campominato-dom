ed18ef2934feb2e197c54b18d53ef01449bba428
Marco Braccini <marco.braccini@live.it>
2022-07-14 11:56:22 +0200

    @INIT Project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# js-campominato-dom
+PROGRAMMA
+
+Seleziono la difficoltà corrispondente al valore dei quadratini massimi da creare per riga/colonna (radice quadrata)
+
+Clicco sul bottone per generare le caselle
+
+Generare un array di 16 numeri casuali verificando che non sono ripetuti (ciclo while) e assegnare una classe perdita
+
+Appendo le caselle con un ciclo definito
+Con "this" definisco in quale punto ho cliccato
+SE contiene la classe perdita
+    il gioco è finito
+ALTRIMENTI
+    conta le caselle che clicco per incrementare il punteggio
---
+++ js/main.js
@@ -0,0 +1,42 @@
+let gridContainer = document.getElementById('grid-container');
+const gameMode = document.getElementById('game-mode');
+const playBtn = document.getElementById('play-btn');
+
+let gridSelection;
+
+playBtn.addEventListener('click', 
+function() {
+    if (gameMode.value == ''){
+        alert('Scegli la difficoltà di gioco.');
+    } else {
+        gridSelection = gameMode.value;
+
+        gridContainer.innerHTML = '';
+    
+        for (let gridNumber = 1; gridNumber <= gridSelection; gridNumber++) {
+    
+            let grid = document.createElement('div');
+            grid.append(gridNumber);
+            grid.classList.add('grid-main');
+            gridContainer.append(grid);
+        
+            grid.addEventListener('click', 
+            function() {
+                console.log(gridNumber);
+        
+                this.classList.toggle('grid-sec');
+            });
+        }
+    
+        let gridMain = document.querySelectorAll('.grid-main');
+        const squareRoot = Math.sqrt(gridSelection);
+    
+        const gridSizePerc = gridContainer.offsetHeight / squareRoot;
+        const gridSizeFinal = ((gridSizePerc * 100) / gridContainer.offsetHeight) + '%';
+    
+        for (let gridList = 0; gridList < gridMain.length; gridList++) {
+            gridMain[gridList].style.width = gridSizeFinal;
+            gridMain[gridList].style.height = gridSizeFinal;
+        }
+    }
+});
